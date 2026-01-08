e554195d05dbe2e114a77a49a95e864420450cf4
punarjee <punarjeehemasinghe2003@gmail.com>
2026-01-08 23:18:34 +0530

    Adding files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './component/Navbar'
 import Footer from './component/Footer'
 import Home from './Pages/Home'
+import Member1 from './Pages/Member4/member4'
 
 const App = () => {
   return (
