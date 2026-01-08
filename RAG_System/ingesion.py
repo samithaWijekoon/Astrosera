ce17176a694474d9e7eed86a5c52530fba6e23f6
Nipun Nirman <164348907+nipunnirman@users.noreply.github.com>
2026-01-08 12:48:01 +0530

    install dependencis

--- RAG_System/ingesion.py
+++ RAG_System/ingesion.py
@@ -1,1 +1,8 @@
-print("hallo")
+import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+
+load_dotenv()
