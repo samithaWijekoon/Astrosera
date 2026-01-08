096ee091092f3324a776ad58c061093f83563bff
Nipun Nirman <164348907+nipunnirman@users.noreply.github.com>
2026-01-08 13:00:23 +0530

    load nasa scraped txt files
    loading nasa documents and vertual environment . error handling. (when file is not docs ,raised error. load only txt file.

--- RAG_System/ingesion.py
+++ RAG_System/ingesion.py
@@ -6,3 +6,35 @@ from langchain_chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# load documents
+
+def load_documents(docs_path="docs"):
+    """Load all text files from the docs directory"""
+    print(f"Loading documents from {docs_path}...")
+    
+    # Check if docs directory exists
+    if not os.path.exists(docs_path):
+        raise FileNotFoundError(f"The directory {docs_path} does not exist. Please create it and add your nasa documents.")
+    
+    # Load all .txt files from the docs directory
+    loader = DirectoryLoader(
+        path=docs_path,
+        glob="*.txt",
+        loader_cls=TextLoader
+    )
+    
+    documents = loader.load()
+    
+    if len(documents) == 0:
+        raise FileNotFoundError(f"No .txt files found in {docs_path}. Please add your company documents.")
+    
+   
+    for i, doc in enumerate(documents[:2]): 
+        print(f"\nDocument {i+1}:")
+        print(f"  Source: {doc.metadata['source']}")
+        print(f"  Content length: {len(doc.page_content)} characters")
+        print(f"  Content preview: {doc.page_content[:100]}...")
+        print(f"  metadata: {doc.metadata}")
+
+    return documents
