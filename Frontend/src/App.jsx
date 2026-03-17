f051ddef5f862cb1beebe364d4d70e4282ada387
Weerasinghe <niwejahesalni@gmail.com>
2026-03-17 20:02:20 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,12 +6,14 @@ import Footer from './component/Footer'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
+import Member2 from './Pages/Member2/member2'
 import Member6 from './Pages/Member6/member6'
 import Member3 from './Pages/Member03/member3'
 import Member4 from './Pages/Member4/Achievment'
 import AdminDashboard from './Pages/Member05/AdminDashboard';
 import Chat from './Pages/chat/chat' // Keep this import!
 import Profile from './Pages/Profile'
+import AsteroidAlerts from './Pages/AsteroidAlerts/AsteroidAlerts'
 
 const App = () => {
   return (
@@ -24,11 +26,12 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
         
-        <Route path="/events" element={<Home />} />
+        <Route path="/events" element={<Member2 />} />
         <Route path="/quiz" element={<Member3 />} />
         <Route path="/achievements" element={<Member4 />} />
         <Route path="/analytics" element={<Home />} />
         <Route path="/news" element={<Member6 />} />
+        <Route path="/asteroid-alerts" element={<AsteroidAlerts />} />
         
         {/* Member 05 - Admin & Analytics Dashboard */}
         <Route path="/admin" element={<AdminDashboard />} />
