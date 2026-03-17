b96ba19d30d09b876cab2e303243931907273162
Weerasinghe <niwejahesalni@gmail.com>
2026-03-17 20:23:10 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './index.css'
 import App from './App.jsx'
+import { UserProvider } from './context/UserContext.jsx'
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID';
 
@@ -11,7 +14,10 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
       <BrowserRouter>
-        <App />
+        <UserProvider>
+          <App />
+          <ToastContainer theme="dark" position="bottom-right" />
+        </UserProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
